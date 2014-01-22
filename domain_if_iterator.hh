69c38620b2655ec6b6434b22448332fdfae428ad
7h1b0 <wyrm1992@hotmail.fr>
2014-01-23 00:21:32 +0100

    Correction next()

--- domain_if_iterator.hh
+++ domain_if_iterator.hh
@@ -12,22 +12,19 @@ public:
 		i_ = -1;
 		max_x = domain_.get_max().x_;
 		max_y = domain_.get_max().y_;
-		std::cout << "coucou" << std::endl;
 	}
 
-	void start(){
+	void start(){ // Fonctionne
 		i_ = 0;	
 		x_ = 0;
-		y_ = 0;
-		while(!domain_.has(point2d(x_,y_))){	
-			i_++;	
+		y_ = 0;;
+		while(  (!domain_.has(point2d(x_,y_)))  ){
+			i_++;
 			x_ = i_ / (max_y + 1);
 			y_ = i_ % (max_y + 1);		
-			std::cout << "hey" << std::endl;		
 		}
 		x_ = i_ / (max_y + 1);
 		y_ = i_ % (max_y + 1);
-		std::cout << "fin start" << std::endl;
 	}
 
 	bool is_valid() const{
@@ -37,11 +34,12 @@ public:
 	void next(){
 		i_++;
 		while(!domain_.has(point2d(x_,y_))){	
-			i_++;					
+			i_++;
+			x_ = i_ / (max_y + 1);
+			y_ = i_ % (max_y + 1);				
 		}
 		x_ = i_ / (max_y + 1);
 		y_ = i_ % (max_y + 1);
-		std::cout << "fin next" << std::endl;
 	}
 
 	operator point2d() const{
