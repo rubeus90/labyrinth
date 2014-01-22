6d1f780eff323e54ed3ab6bff423c89adc85c86c
7h1b0 <wyrm1992@hotmail.fr>
2014-01-23 00:00:12 +0100

    Modification Constructeur

--- domain_if.hh
+++ domain_if.hh
@@ -9,21 +9,18 @@ template <typename D, typename F>
 
 class domain_if{
 public:
+	typedef point2d point_type;
 	typedef domain_if_iterator<domain_if> p_iterator_type;
-	// typedef box2d_iterator<domain_if> p_iterator_type;
 
-	domain_if(D d, F& f): d_(d), f_(f){
-	}
+	domain_if(D d, F& f): d_(d), f_(f), min_(d_.get_min()), max_(d.get_max()){}
 
-	// domain_if(const domain_if& domain): d_(d), f_({
-	// }
 
 	point2d get_min() const{
-		return d_.get_min();
+		return min_;
 	}
 
 	point2d get_max() const{
-		return d_.get_max();
+		return max_;
 	}
 
 	bool has(const point2d& p){
@@ -32,6 +29,7 @@ public:
 private:
 	D& d_;
 	F& f_;
+	point_type min_, max_;
 };
 
 #endif
