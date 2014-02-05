cb6b34290c35d7be4c782d1e779e71b35f45143d
7h1b0 <wyrm1992@hotmail.fr>
2014-02-05 22:50:01 +0100

    Update traceur.hh

--- traceur.hh
+++ traceur.hh
@@ -14,8 +14,28 @@ public:
 		image(pointInit) = pointInit;
 	}
 
-	void follow(point2d pointDepart, point2d pointArrive){
+	template <typename T>
+	void follow(point2d pointDepart, point2d pointArrive, image2d<T>& image){
+		image(pointArrive) = pointDepart;
 	}
+
+	template <typename T>
+  	void affiche(image2d<T>& image){
+  		int nrows_ = image.domain().get_max().x_ +1;
+	    int ncols_ = image.domain().get_max().y_ +1;
+
+	    std::cout << "------------------------" << std::endl << std::endl;
+	    for(int j=0; j< nrows_; j++){
+	      	for(int i=0; i< ncols_; i++){
+		        unsigned x = (i+j*ncols_) / ncols_;
+		        unsigned y = (i+j*ncols_) % ncols_;
+		        point2d p(x,y);
+		       	std::cout << image(p).x_ << " " << image(p).y_ << "\t";
+		    }
+	     	std::cout << std::endl;
+	    }
+	    std::cout << std::endl << "------------------------" << std::endl;
+  	}
 
 private:
 	D domain_;
