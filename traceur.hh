fe232e5221d90f2cc5507e599371821486eac454
7h1b0 <wyrm1992@hotmail.fr>
2014-02-05 22:15:56 +0100

    Update traceur.hh

--- traceur.hh
+++ traceur.hh
@@ -3,26 +3,24 @@
 
 #include "image2d.hh"
 
-template <typename D>
+template <typename D, typename F>
 
 class Traceur{
 public:
-	Traceur(D domain): domain_(domain), image_(6,5) {
-	}
+	Traceur(D domain): domain_(domain), image_(6,5) {}
 
 	void init(point2d pointInit){
-		// std::cout << image(pointInit).x_ << "        HEYYYYYYY" << std::endl;
-		image_(pointInit) = 1;
-		// image(point2d(0,0)) = point2d(0,0);
+		image_(pointInit) = 4;
 	}
 
 	void follow(point2d pointDepart, point2d pointArrive){
-		// image_(pointArrive) = pointDepart;
+		//std::cout << "I give you everything my love !" << std::endl;
+		//image_(pointArrive) = pointDepart;
 	}
 
 private:
 	D domain_;
-	image2d<int> image_;
+	F image_;
 };
 
 #endif
